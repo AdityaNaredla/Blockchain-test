3dae8bada0bb98537be3997bb4392a40b98bcfec
AdityaNaredla <naredlavidhatha@gmail.com>
2026-05-01 11:30:06 -0500

    Refactor RTC_CONFIG for TURN server settings
    Updated TURN server configuration and removed deprecated entries.

--- client/src/lib/peer.js
+++ client/src/lib/peer.js
@@ -20,20 +20,16 @@ const RTC_CONFIG = {
   iceServers: [
     { urls: "stun:stun.l.google.com:19302" },
     { urls: "stun:stun1.l.google.com:19302" },
+    { urls: "stun:stun.cloudflare.com:3478" },
     {
-      urls: "turn:openrelay.metered.ca:80",
-      username: "openrelayproject",
-      credential: "openrelayproject",
-    },
-    {
-      urls: "turn:openrelay.metered.ca:443",
-      username: "openrelayproject",
-      credential: "openrelayproject",
-    },
-    {
-      urls: "turn:openrelay.metered.ca:443?transport=tcp",
-      username: "openrelayproject",
-      credential: "openrelayproject",
+      urls: [
+        "turn:global.relay.metered.ca:80",
+        "turn:global.relay.metered.ca:80?transport=tcp",
+        "turn:global.relay.metered.ca:443",
+        "turns:global.relay.metered.ca:443?transport=tcp",
+      ],
+      username: "e8dd65b92c62d3e36cafb807",
+      credential: "uWdWNmkhvyqTEswO",
     },
   ],
   iceCandidatePoolSize: 10,
