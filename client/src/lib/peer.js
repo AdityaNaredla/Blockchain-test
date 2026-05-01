21eda98212f1aff059b363195fe25f7976262edc
AdityaNaredla <naredlavidhatha@gmail.com>
2026-05-01 11:23:05 -0500

    Add additional STUN and TURN servers for RTC

--- client/src/lib/peer.js
+++ client/src/lib/peer.js
@@ -17,7 +17,26 @@ import { getWsBase } from "./api";
 // Use Google's public STUN server for NAT traversal
 // (Production deployments would also need a TURN server for symmetric NATs)
 const RTC_CONFIG = {
-  iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+  iceServers: [
+    { urls: "stun:stun.l.google.com:19302" },
+    { urls: "stun:stun1.l.google.com:19302" },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443?transport=tcp",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+  ],
+  iceCandidatePoolSize: 10,
 };
 
 export class PeerManager {
